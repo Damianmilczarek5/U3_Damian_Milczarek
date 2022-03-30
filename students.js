a26994e93987c0f084dfef09421666b222fb55db
Damianmilczarek5 <damian.milczarek2000@gmail.com>
2022-03-30 19:11:37 +0200

    created credits function

--- students.js
+++ students.js
@@ -2,12 +2,15 @@
 // created a variable to pull out all students
 let allStudents = DATABASE.students
 
+//function that renders a student
 function renderStudent(id) {
 let div = document.createElement("div");
 let student = DATABASE.students[id];
+//makes a container for the student
 div.id = "container"
+// html of the students container
 div.innerHTML = `
-<header>${student.firstName} ${student.lastName} (total: ${completeCredits(student)} credits)</header>
+<header>${student.firstName} ${student.lastName} (total: ${credits(student)} credits)</header>
 <div>
         <div id="course">
             <h3>Courses:</h3>
@@ -18,3 +21,17 @@ div.innerHTML = `
     </div>`
     return div
 }
+
+// function that renders stutends total credits 
+function credits (student){
+    let credits = []
+    for (let course of student.courses){
+        credits.push(course.passedCredits)
+    }
+    let creditsSum = 0
+    for (let i = 0; i < credits.length; i++){
+        creditsSum += credits[i]
+    }
+    return creditsSum
+}
+
