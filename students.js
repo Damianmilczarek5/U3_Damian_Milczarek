296ba94a7e4643f21ec1a8be508d584a5320812d
Damianmilczarek5 <damian.milczarek2000@gmail.com>
2022-03-31 18:57:48 +0200

    Squashed commit of the following:
    commit 3726ce575bfde42c3385e34bcde39e103ed8451c
    Author: Damianmilczarek5 <damian.milczarek2000@gmail.com>
    Date:   Thu Mar 31 18:21:14 2022 +0200
    
        added filter and a function that renders all courses

--- students.js
+++ students.js
@@ -79,14 +79,14 @@ function renderCourses (student){
 // function for when user types in search box it filters trought possible hits
 function studentLastname (){
     let studentsArray = []
-    for (let i = 0; i < allStudents.length; i++){
+    for ( let i = 0; i < allStudents.length; i++){
         document.querySelector("#wrapper").innerHTML = ""
-        if ("" == searchWord()) {
+        if ("" == searchWord()){
             document.querySelector("#wrapper").innerHTML = ""
-        }
-        else if (allStudents[i].lastName.toLocaleLowerCase().includes(searchWord())) {
+        } else if (allStudents[i].lastName.toLocaleLowerCase().includes(searchWord())) {
             studentsArray.push(allStudents[i]);
-        }
+        } 
+
     }
     renderStudents(studentsArray)
 }
